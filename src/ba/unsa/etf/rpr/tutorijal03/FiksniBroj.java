e019345a1d03e2ee30841c53c90dd55becb27889
asuljagic <43003728+asuljagic@users.noreply.github.com>
2018-10-28 20:57:39 +0100

    Implementirana klasa FiksniBroj!

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -5,12 +5,12 @@ import java.util.Objects;
 public class FiksniBroj extends TelefonskiBroj{
     public enum Grad{SARAJEVO,ZENICA,TUZLA,ORASJE,BIHAC,TRAVNIK,
         LIVNO, SIROKIBRIJEG,MOSTAR,BRCKO, GORAZDE};
-    private String broj;
     private Grad grad;
+    private String broj;
 
-    public FiksniBroj(String broj, Grad grad) {
-        this.broj = broj;
+    public FiksniBroj(Grad grad, String broj) {
         this.grad = grad;
+        this.broj = broj;
     }
 
     @Override
