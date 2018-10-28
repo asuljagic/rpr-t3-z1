b61d9d8c1b5b1b9c85c652f9d14c70177b04b4e1
asuljagic <43003728+asuljagic@users.noreply.github.com>
2018-10-28 20:27:34 +0100

    Dodana apstraktna klasa!

---
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr.tutorijal03;
+//apstraktna klasa
+public class TelefonskiBroj {
+    String broj;
+
+    public abstract String ispisi();
+    public abstract int hashCode();
+}
