90e4df2bb556f781624f371e9fa3a47bc46f1290
asuljagic <43003728+asuljagic@users.noreply.github.com>
2018-10-30 20:29:33 +0100

    Dodano compareTo

--- src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -1,6 +1,6 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public class MedunarodniBroj {
+public class MedunarodniBroj extends TelefonskiBroj {
     private String drzava;
     private String broj;
 
@@ -14,9 +14,35 @@ public class MedunarodniBroj {
         return 0;
     }
 
+    public String getDrzava() {
+        return drzava;
+    }
+
+    public void setDrzava(String drzava) {
+        this.drzava = drzava;
+    }
+
+    public String getBroj() {
+        return broj;
+    }
+
+    public void setBroj(String broj) {
+        this.broj = broj;
+    }
+
     @Override
     public String ispisi(){
-        return drzava + "/" + broj;
+        return this.drzava + "/" + this.getBroj();
+    }
+    @Override
+    public String toString(){
+        return this.ispisi();
+    }
+
+    @Override
+    public int compareTo(Object o){                                                  // compareTo override za poredenje
+        FiksniBroj broj = (FiksniBroj) o;
+        return this.ispisi().compareTo(broj.ispisi());
     }
 
 }
